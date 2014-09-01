fd5183ae0e14f3769fe2badfced4bbd9f475368b
Dervol03 <b_d_v@web.de>
2014-09-01 21:07:35 +0200

    Browser#field now also works when the label is passed as Watir::Element

--- lib/watir_formhandler/browser.rb
+++ lib/watir_formhandler/browser.rb
@@ -1,10 +1,11 @@
 module Watir
   class Browser
     # Searches for the specified label and returns the form field belonging to it, identified by the
-    # 'for' attribute of the label.
-    # @param [String] label the label for which to find the form field.
+    # 'for' attribute of the label. Alternatively, you may pass a Watir::Label.
+    # @param [String, Watir::Label] label the label for which to find the form field.
+    # @return [Watir::Element] form field of the given label.
     def field(label)
-      element(id: label(text: label).for)
+      label.respond_to?(:for) ? element(id: label.for) : element(id: label(text: label).for)
     end
 
 
--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -3,12 +3,18 @@ require 'spec_helper'
 
 module Watir
   describe Browser do
+    let(:browser){ @browser }
     describe '#field' do
-      before(:each) { @browser.goto(local_url(FORM_PAGE))}
+      before(:each) { browser.goto(local_url(FORM_PAGE))}
 
       describe 'without a start node' do
         it 'returns the form field of the specified label string' do
-          expect(@browser.field('Checkbox').id).to eq('checkbox')
+          expect(browser.field('Checkbox').id).to eq('checkbox')
+        end
+
+        it 'returns the form field of the given label node' do
+          field_label = browser.label(text: 'Checkbox')
+          expect(browser.field(field_label).id).to eq('checkbox')
         end
       end
     end
