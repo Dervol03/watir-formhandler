993c60a77988493ee57da69a7f4ccfa14f86897c
Dervol03 <b_d_v@web.de>
2014-09-02 21:30:19 +0200

    Adatped Watir::Container and Watir::Element to properly generate Watir::OptionGroup elements

--- lib/watir_formhandler.rb
+++ lib/watir_formhandler.rb
@@ -2,5 +2,10 @@ require 'watir'
 require 'watir-webdriver'
 require 'selenium-webdriver'
 
-require 'watir_formhandler/browser'
+require 'watir_formhandler/watir'
+require 'watir_formhandler/element'
 require 'watir_formhandler/option_group'
+require 'watir_formhandler/container'
+require 'watir_formhandler/browser'
+
+
--- lib/watir_formhandler/option_group.rb
+++ lib/watir_formhandler/option_group.rb
@@ -1,6 +1,20 @@
 module Watir
-  class OptionGroup < Element
-    include Enumerable
+
+  # The OptionGroup represents the parent node of any grouped checkboxes or radio buttons. Its
+  # purpose is to provide a way to select different options in a more natural way, like the user
+  # would do it: instead of going through all checkboxes one by one and setting them to 'true',
+  # OptionGroup allows to specify the option names that are desired to be opted in.
+  # @example
+  #   group = browser.option
+  class OptionGroup < HTMLElement
+
+    def initialize(parent, selector)
+      stripped_selector = selector.respond_to?(:selector) ? selector.selector : selector
+      super parent, stripped_selector
+
+      @element = selector if selector.respond_to?(:selector)
+    end
+
 
   end
 end
