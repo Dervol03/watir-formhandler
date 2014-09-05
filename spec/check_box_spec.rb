c0a23cf270de89274e582caf949b403aabc73a2d
Dervol03 <b_d_v@web.de>
2014-09-05 23:48:52 +0200

    Refactored check_box_spec.rb to work without the browser

--- spec/check_box_spec.rb
+++ spec/check_box_spec.rb
@@ -2,40 +2,37 @@ require 'spec_helper'
 
 module Watir
   describe CheckBox do
-    before(:all)    { @browser = Browser.new              }
-    after(:all)     { @browser.close                      }
-    before(:each)   { @browser.goto(local_url(FORM_PAGE)) }
-    let(:checkbox)  { @checkbox                           }
+    before(:each)   { @checkbox = Watir::CheckBox.new('', id: '') }
+    let(:checkbox)  { @checkbox                                   }
 
     describe '#set' do
-      before(:each) { @checkbox = @browser.checkboxes.first }
 
       context 'when given true' do
         it 'checks it if unchecked' do
-          checkbox.click if checkbox.checked?
+          allow(checkbox).to receive(:checked?).and_return(false)
+          expect(checkbox).to receive(:click)
           checkbox.set(true)
-          expect(checkbox).to be_checked
         end
 
         it 'does not check it if checked' do
-          checkbox.click unless checkbox.checked?
+          allow(checkbox).to receive(:checked?).and_return(true)
+          expect(checkbox).to_not receive(:click)
           checkbox.set(true)
-          expect(checkbox).to be_checked
         end
       end
 
 
       context 'when given false' do
         it 'unchecks it if checked' do
-          checkbox.click unless checkbox.checked?
+          allow(checkbox).to receive(:checked?).and_return(true)
+          expect(checkbox).to receive(:click)
           checkbox.set(false)
-          expect(checkbox).to_not be_checked
         end
 
         it 'does not uncheck it if unchecked' do
-          checkbox.click if checkbox.checked?
+          allow(checkbox).to receive(:checked?).and_return(false)
+          expect(checkbox).to_not receive(:click)
           checkbox.set(false)
-          expect(checkbox).to_not be_checked
         end
       end
     end
