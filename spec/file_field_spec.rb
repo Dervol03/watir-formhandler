1539b636da1dc681a18e5d497511a12fa570dee4
Dervol03 <b_d_v@web.de>
2014-09-13 23:10:52 +0200

    Implemented FileField#set_value refs #6 @0.2

---
+++ spec/file_field_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+module Watir
+  describe FileField do
+    describe '#set_value' do
+      subject { FileField.new('', id: '') }
+
+      context 'no path is set' do
+        it 'returns empty string' do
+          allow(subject).to receive(:value).and_return(nil)
+          expect(subject.set_value).to eq('')
+        end
+      end
+
+      context 'a path is set' do
+        it 'returns string with file name' do
+          allow(subject).to receive(:value).and_return('form.html')
+          expect(subject.set_value).to eq('form.html')
+        end
+      end
+    end
+  end
+end
