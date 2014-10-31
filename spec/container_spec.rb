11d828551c17630f29c058befa76c4b1d3e5572c
Dervol03 <b_d_v@web.de>
2014-10-31 08:23:37 +0100

    Did some RSpec refactoring for Container class

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -176,17 +176,19 @@ module Watir
         end
 
         it 'fills a Watir::Select with multiple select' do
-          browser.fill_in('Multi Select', %w(Option2 Option3))
+          options = %w(Option2 Option3)
+          browser.fill_in('Multi Select', options)
           multiselect = browser.select(id: 'multiselect')
           expect(multiselect.selected_options.count).to eq(2)
-          expect(multiselect.selected_options.map(&:text)).to eq(%w(Option2 Option3))
+          expect(multiselect.selected_options.map(&:text)).to eq(options)
         end
 
         it 'fills a Watir::OptionGroup' do
-          browser.fill_in('Checkbox5', %w(Checkbox5 Radio7), include_groups: true)
+          options = %w(Checkbox5 Radio7)
+          browser.fill_in('Checkbox5', options, include_groups: true)
           option_group = browser.option_group(id: 'radio_and_checkbox')
           expect(option_group.selected_options.count).to eq(2)
-          expect(option_group.selected_options).to eq(%w(Checkbox5 Radio7))
+          expect(option_group.selected_options).to eq(options)
         end
       end # desribe: without a start node
 
@@ -196,65 +198,60 @@ module Watir
         let(:start_node){ browser.element(id: 'main_content') }
 
         it 'fills a Watir::Checkbox from start node' do
-          allow(start_node).to receive(:label).and_return(browser.label(text: 'Checkbox'))
-          expect(start_node).to receive(:label)
+          expect(start_node).to receive(:label).and_return(browser.label(text: 'Checkbox'))
           browser.fill_in('Checkbox', true, start_node: start_node)
           expect(browser.checkboxes.first).to be_checked
         end
 
         it 'fills a Watir::Radio from start node' do
-          allow(start_node).to receive(:label).and_return(browser.label(text: 'Radio'))
-          expect(start_node).to receive(:label)
+          expect(start_node).to receive(:label).and_return(browser.label(text: 'Radio'))
           browser.fill_in('Radio', true, start_node: start_node)
           expect(browser.radios.first).to be_checked
         end
 
         it 'fills a Watir::Select  with single select from start node' do
-          allow(start_node).to receive(:label).and_return(browser.label(text: 'Select'))
-          expect(start_node).to receive(:label)
+          expect(start_node).to receive(:label).and_return(browser.label(text: 'Select'))
           browser.fill_in('Select', 'Test2', start_node: start_node)
           expect(browser.selects.first.selected_options.count).to eq(1)
           expect(browser.selects.first.selected_options.first.text).to eq('Test2')
         end
 
         it 'fills a Watir::Select with multiple select from start node' do
-          allow(start_node).to receive(:label).and_return(browser.label(text: 'Multi Select'))
-          expect(start_node).to receive(:label)
-          browser.fill_in('Multi Select', %w(Option2 Option3), start_node: start_node)
+          options = %w(Option2 Option3)
+          expect(start_node).to receive(:label).and_return(browser.label(text: 'Multi Select'))
+          browser.fill_in('Multi Select', options, start_node: start_node)
+
           multiselect = browser.select(id: 'multiselect')
           expect(multiselect.selected_options.count).to eq(2)
-          expect(multiselect.selected_options.map(&:text)).to eq(%w(Option2 Option3))
+          expect(multiselect.selected_options.map(&:text)).to eq(options)
         end
 
         it 'fills a Watir::TextField from start node' do
-          allow(start_node).to receive(:label).and_return(browser.label(text: 'Text Field'))
-          expect(start_node).to receive(:label)
+          expect(start_node).to receive(:label).and_return(browser.label(text: 'Text Field'))
           browser.fill_in('Text Field', 'test text', start_node: start_node)
           expect(browser.text_fields.first.value).to eq('test text')
         end
 
         it 'fills a Watir::TextArea from start node' do
-          allow(start_node).to receive(:label).and_return(browser.label(text: 'Text Area'))
-          expect(start_node).to receive(:label)
+          expect(start_node).to receive(:label).and_return(browser.label(text: 'Text Area'))
           browser.fill_in('Text Area', 'test text', start_node: start_node)
           expect(browser.textareas.first.value).to eq('test text')
         end
 
         it 'fills a Watir::FileField from start node' do
-          allow(start_node).to receive(:label).and_return(browser.label(text: 'File'))
-          expect(start_node).to receive(:label)
+          expect(start_node).to receive(:label).and_return(browser.label(text: 'File'))
           browser.fill_in('File', File.join(HTML_DIR, FORM_PAGE), start_node: start_node)
           expect(browser.file_fields.first.value).to eq(FORM_PAGE)
         end
 
         it 'fills a Watir::OptionGroup from start node' do
+          options = %w(Checkbox5 Radio7)
           expect(start_node).to receive(:label).and_return(browser.label(text: 'Checkbox5'))
-          browser.fill_in(
-            'Checkbox5', %w(Checkbox5 Radio7), include_groups: true, start_node: start_node
-          )
+          browser.fill_in('Checkbox5', options, include_groups: true, start_node: start_node)
+
           option_group = browser.option_group(id: 'radio_and_checkbox')
           expect(option_group.selected_options.count).to eq(2)
-          expect(option_group.selected_options).to eq(%w(Checkbox5 Radio7))
+          expect(option_group.selected_options).to eq(options)
         end
       end # descibe: with a start node
 
